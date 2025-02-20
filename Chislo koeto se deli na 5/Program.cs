c5f748a898619a87cd4d5bd9c69a08b39fc94156
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-20 15:58:34 +0200

    Add project files.

---
+++ Chislo koeto se deli na 5/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine());
+
+if (a % 5 == 0)
+{
+
+    Console.WriteLine("Moje da se deli na 5");
+
+}
+
+else
+{
+    Console.WriteLine("Ne moje da se deli na 5");
+}
